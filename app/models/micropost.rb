9c2d7c2cd01518a9e283566908d2294fae7d5005
Sylpha777 <sylpha.ponrena@gmail.com>
2021-04-15 00:57:44 +0000

    favorite

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,4 +4,7 @@ class Micropost < ApplicationRecord
   
   validates :content, presence: true, length: { maximum: 255 }
   
+  has_many :reverses_of_favorite, class_name: 'Favorite', foreign_key: 'micropost_id'
+  has_many :favorite_users, through: :reverses_of_favorite, source: :user
+  
 end
